c85c7b335b9e9835907b5072d05b4b9b218387b9
Avinash Kumar <www.avinashkumar2@gmail.com>
2020-06-08 16:22:59 +0530

    hbs done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const hbs = require('hbs');
 const session=require("express-session");
 const app=express();
 
-app.use(session({secret:"1234asdf",resave:false, saveUninitialized:true}))
+app.use(session({secret:"1234asdf",resave:false, saveUninitialized:false}))
 app.set('view engine', 'hbs');
 
 ///     DATABASE CONNECTION     ///
@@ -18,24 +18,43 @@ app.use(express.static('images'));
 app.use(express.static('css'));  //css files
 app.use(bodyparser.urlencoded({extended:true}));
 app.get("/", function(req,res){
-    //res.sendFile(__dirname+"/index.html")
+    //result.sendFile(__dirname+"/index.html")
     res.render("main")
 })//  e:\rentingSystem\backend
 
 // verfying user 
 app.post("/login", function(req,res){
-  let username=parseInt(req.body.name);   // username
-  let pswd=req.body.password;
-  let name;   // password
-  console.log("Username: "+username," password: "+pswd)
+  var userid=parseInt(req.body.name);   // username
+  var pswd=req.body.password;   // password
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var d = new Date();
+  var date=  days[d.getDay()]+" "+ d.getDate()+"-"+months[d.getMonth()]+"-"+d.getFullYear();
+  //data
+
+  var name;
+  var water;
+  var baserent;
+  var security;
+  var electricity;
+  var maintenance;
+  var approvCount;
+  var aprov;
+  var pendPay;
+  var pendPayCount;
+  var recPay;
+  var recPayCount;
+  var totaltenant;
+
+ // console.log("Username: "+username," password: "+pswd)
   MongoClient.connect(url, {useUnifiedTopology:true},function(err,db){
     if(err) throw err;
     var dbo=db.db("renting");
-    dbo.collection("landlord").find( {landlordID:username,pswd: pswd},).toArray(function(err,result){
+    dbo.collection("landlord").find( {landlordID:userid,pswd: pswd},).toArray(function(err,result){
       if(err) throw err;
       if(result.length==0)
       {
-        console.log("User not found res: ")
+        console.log("User not found result: ")
         res.render("main",{message: "INCORRECT credentials"})
       }
       else{
@@ -44,14 +63,89 @@ app.post("/login", function(req,res){
         console.log("user found successful ");
         name=result[0].fname;
         console.log("name: "+name)
-        res.render("land",{username:result[0].fname, userid:username })
+        console.log(result);
+        
+        ///---------- FILLING RESULTS-------------////////////////////
+          //name=result[0].fname;
+          baserent=result[0].baserent
+          water= result[0].water
+          electricity=result[0].electricity
+          security=result[0].security
+          maintenance=result[0].maintenance
       }
     })
+    console.log("req ses uid: "+req.session.userid)
+    if(req.session.userid==undefined)
+    {     console.log("getting rest all data")
+          // pending approval count
+    dbo.collection("tenant").find({verified:false}).count(function(err,result){
+      if (err) throw err;
+      console.log("Pending approval Tenants count  "+result);
+      approvCount=result;
+    })  //user approvals name
+    dbo.collection("tenant").find({verified:false}, {projection: {fname:1, _id:0}}).toArray(function(err,result){
+      if (err) throw err;
+      console.log("Pending approval Tenants list: ");
+      console.log(result);
+      aprov=result;
+      //console.log(aprov)
+    }) // pending payments
+    dbo.collection("transaction").find( {landlordID:userid , paidON: null}, {projection: {tid:1, tenantID:1, _id:0}}).toArray( function(err,result){
+      if(err) throw err;
+      console.log("pending payments : ")
+      console.log(result)
+      pendPay=result;
+      console.log("pending payments COUNT : ")
+      console.log(result.length)
+      pendPayCount=result.length;
+    })
+    // recieved payments
+    dbo.collection("transaction").find( {paidON: {$ne:null}}, {projection: {tid:1, tenantID:1, _id:0}}).toArray( function(err,result){
+      if(err) throw err;
+      console.log("Recieved payments: ")
+      console.log(result)
+      recPay=result;
+      console.log("RECIEVED payments COUNT : ")
+      console.log(result.length)
+      recPayCount=result.length;
+    })
+    dbo.collection("tenant").find ({ verified: true}).count(function(err,result){
+      console.log("Total verified Tenants: ")
+      console.log(result);
+      totaltenant=result;
+
+      //  data populating 
+  res.render("land",
+  {
+    username:name,
+    userid:userid ,
+    baserent:baserent,
+    water:water,
+    electricity:electricity,
+    security:security,
+    maintenance:maintenance,
+    date:date,
+    recpaycount:recPayCount,
+    penpaycount:pendPayCount,
+    totalprofit:"",
+    tenantcount:totaltenant,
+    approvalcount:approvCount,
+    pendpay:pendPay,
+    aprov:aprov,
+    recpay:recPay
   })
+    })
+
+  }
+
+    
+  })
+  
+  
 })
 
 
 ///  LISITING SERVER  DONT EDIT   //
-app.listen(3000, function(req,res){
+app.listen(3000, function(req,result){
     console.log(" Server up and running:: http://localhost:3000")
 })
