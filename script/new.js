b6236c026cfd714bc87d38c9448f86dad344452a
Avinash Kumar <www.avinashkumar2@gmail.com>
2020-08-07 07:33:31 +0530

    Landlord checking done p-2

--- script/new.js
+++ script/new.js
@@ -3,6 +3,12 @@ console.log("Script for tenant login , landlord exist check")
 
 async function checkLandlord() {
     var lid=document.getElementById('landlordinp').value;
+    console.log(lid)
+    if(lid==="") {
+        document.getElementById("exist").style.display="none";
+        document.getElementById("notexist").style.display="none";
+        return;
+    }
     var returned=await fetch("/landlordcheck?lid="+lid)
     returned.json()
         .then(d=>{
